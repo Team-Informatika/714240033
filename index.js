8a69ca14587a431bb44a471ce13476e737c470f5
KucingNanda <nandasr.24@gmail.com>
2025-01-31 16:59:13 +0700

    update2

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-import { renderHTML } from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.0/element.js";
+import { renderHTML,setInner } from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.0/element.js";
 import { getJSON } from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.0/api.js";
+
+renderHTML('content', 'content/utama.html');
 
 getJSON("https://t.if.co.id/json/kucingabu.json", null, null, responseFunction);
 
 function responseFunction(response) {
   console.log("HTTP Status:", response.status);
   console.log("Response Data:", response.data);
+
+  if (response.status === 200) {
+    const data = response.data;
+    const content = `
+        <p>Deskripsi: ${data.deskripsi}</p>
+    `;
+
+    setInner("kartu", content);
+  } else {
+    setInner("kartu", `<p>Error: Error Coy.</p>`);
+  }
 }
