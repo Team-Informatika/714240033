f3c8a66840ec851bcf5c5f3e79850a1b18a104d3
KucingNanda <nandasr.24@gmail.com>
2025-01-31 16:10:57 +0700

    update1

---
+++ index.js
@@ -0,0 +1,9 @@
+import { renderHTML } from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.0/element.js";
+import { getJSON } from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.0/api.js";
+
+getJSON("https://t.if.co.id/json/kucingabu.json", null, null, responseFunction);
+
+function responseFunction(response) {
+  console.log("HTTP Status:", response.status);
+  console.log("Response Data:", response.data);
+}
