2cf67fbac252bd3959a263846e7b746fce486a99
KucingNanda <nandasr.24@gmail.com>
2025-02-06 12:46:40 +0700

    update3

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { getJSON } from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.0/api.js";
 
 renderHTML('content', 'content/utama.html');
 
-getJSON("https://t.if.co.id/json/kucingabu.json", null, null, responseFunction);
+getJSON("https://t.if.co.id/json/kucingabu.json", "null", "null", responseFunction);
 
 function responseFunction(response) {
   console.log("HTTP Status:", response.status);
@@ -11,12 +11,4 @@ function responseFunction(response) {
 
   if (response.status === 200) {
     const data = response.data;
-    const content = `
-        <p>Deskripsi: ${data.deskripsi}</p>
-    `;
-
-    setInner("kartu", content);
-  } else {
-    setInner("kartu", `<p>Error: Error Coy.</p>`);
-  }
 }
