7743d9d47b112f503c7df69254f0d3dfd2279a11
KucingNanda <nandasr.24@gmail.com>
2025-02-20 17:14:34 +0700

    update14

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-import { renderHTML, setInner } from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.0/element.js";
+import {
+  renderHTML,
+  setInner,
+} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.0/element.js";
 import { getJSON } from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.0/api.js";
 
-renderHTML('kartu', 'content/render.html', renderKartu);
+renderHTML("kartu", "content/render.html", renderKartu);
 
 function renderKartu() {
-  getJSON("https://t.if.co.id/json/kucingabu.json", "null", "null", responseFunction);
+  getJSON(
+    "https://t.if.co.id/json/kucingabu.json",
+    "null",
+    "null",
+    responseFunction
+  );
 }
 
 function responseFunction(response) {
@@ -12,15 +20,19 @@ function responseFunction(response) {
   console.log("Response Data:", response.data);
   const imageSrc = response.data.card.image.src;
   const imageHTML = `<img src="${imageSrc}" alt="PPFormal">`;
-  setInner('image', imageHTML);
-  setInner('name', response.data.card.name);
-  setInner('username', response.data.card.username);
-  setInner('description', response.data.card.description);
-  setInner('rate', response.data.card.rate);
-  // const socialcon = document.getElementById('social')
-  // let social = response.data.card.social_links;
-  // social.forEach((href) => {
-  //   const socialcontainer = document.createElement("a")
-  //   socialcontainer.
-  // });
+  setInner("image", imageHTML);
+  setInner("name", response.data.card.name);
+  setInner("username", response.data.card.username);
+  setInner("description", response.data.card.description);
+  setInner("rate", response.data.card.rate);
+  const socialcon = document.getElementById("social");
+  let social = response.data.card.social_links;
+  social.forEach((href) => {
+    const socialcontainer = document.createElement("a");
+    socialcontainer.textContent = href.platform;
+    socialcontainer.href = href.link;
+    socialcontainer.target = "_blank";
+    console.log(socialcontainer);
+    socialcon.appendChild(socialcontainer);
+  });
 }
