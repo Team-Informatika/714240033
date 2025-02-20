8dbef65ba916ff0de629f98060de8357b57566e5
KucingNanda <nandasr.24@gmail.com>
2025-02-20 16:29:05 +0700

    update5

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 import { renderHTML, setInner } from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.0/element.js";
 import { getJSON } from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.0/api.js";
 
-renderHTML('content', 'content/utama.html');
+renderHTML('kartu', 'content/render.html');
 
-getJSON("https://t.if.co.id/json/kucingabu.json", "null", "null", responseFunction);
+function renderKartu() {
+  getJSON("https://t.if.co.id/json/kucingabu.json", "null", "null", responseFunction);
+}
 
 function responseFunction(response) {
   console.log("HTTP Status:", response.status);
   console.log("Response Data:", response.data);
-
-  if (response.status === 200) {
-    const data = response.data;
-}
+  const imageSrc = response.data.card.image.src;
+  const imageHTML = `<img src="${imageSrc}" alt="PPFormal">`;
+  setInner('image', imageHTML);
+  setInner('name', response.data.card.name);
+  setInner('username', response.data.card.username);
+  setInner('description', response.data.card.description);
+  setInner('rate', response.data.card.rate);
+  const social = document.getElementById()
